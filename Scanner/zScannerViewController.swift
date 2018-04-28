550620ed63d12ea6c8f3fb3403aeed9fcb3ed026
Jean-Michel ZARAGOZA <jmz49xp@gmail.com>
2018-04-29 00:35:15 +0200

    version ok 1

--- Scanner/zScannerViewController.swift
+++ Scanner/zScannerViewController.swift
@@ -1,10 +1,12 @@
 //
 //  ViewController.swift
-//  BarcodeScanner
+//  Scanner
 //
-//  Created by Mikheil Gotiashvili on 7/14/17.
-//  Copyright © 2017 Mikheil Gotiashvili. All rights reserved.
+//  Created by jean-michel zaragoza on 24/04/2018.
+//  Copyright © 2018 jean-michel zaragoza. All rights reserved.
 //
+
+
 
 import UIKit
 import AVFoundation
@@ -15,49 +17,49 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
     var captureDevice:AVCaptureDevice?
     var videoPreviewLayer:AVCaptureVideoPreviewLayer?
     var captureSession:AVCaptureSession?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.title = "Scanner"
         view.backgroundColor = .white
-        
+
         captureDevice = AVCaptureDevice.default(for: .video)
         // Check if captureDevice returns a value and unwrap it
         if let captureDevice = captureDevice {
-        
+
             do {
                 let input = try AVCaptureDeviceInput(device: captureDevice)
-                
+
                 captureSession = AVCaptureSession()
                 guard let captureSession = captureSession else { return }
                 captureSession.addInput(input)
-                
+
                 let captureMetadataOutput = AVCaptureMetadataOutput()
                 captureSession.addOutput(captureMetadataOutput)
-                
+
                 captureMetadataOutput.setMetadataObjectsDelegate(self, queue: .main)
                 captureMetadataOutput.metadataObjectTypes = [.code128, .qr, .ean13,  .ean8, .code39] //AVMetadataObject.ObjectType
-                
+
                 captureSession.startRunning()
-                
+
                 videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
                 videoPreviewLayer?.videoGravity = .resizeAspectFill
                 videoPreviewLayer?.frame = view.layer.bounds
                 view.layer.addSublayer(videoPreviewLayer!)
-                
+
             } catch {
                 print("Error Device Input")
             }
-            
+
         }
-        
+
         view.addSubview(codeLabel)
         codeLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         codeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         codeLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
         codeLabel.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -71,7 +73,7 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
         codeLabel.translatesAutoresizingMaskIntoConstraints = false
         return codeLabel
     }()
-    
+
     let codeFrame:UIView = {
         let codeFrame = UIView()
         codeFrame.layer.borderColor = UIColor.green.cgColor
@@ -80,7 +82,7 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
         codeFrame.translatesAutoresizingMaskIntoConstraints = false
         return codeFrame
     }()
-    
+
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         if metadataObjects.count == 0 {
             //print("No Input Detected")
@@ -88,40 +90,40 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
             codeLabel.text = "No Data"
             return
         }
-        
+
         let metadataObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-        
+
         guard let stringCodeValue = metadataObject.stringValue else { return }
-        
+
         view.addSubview(codeFrame)
-        
+
         guard let barcodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObject) else { return }
         codeFrame.frame = barcodeObject.bounds
         codeLabel.text = stringCodeValue
 
         print("++++++++++++++ \(stringCodeValue)")
-        
+
         // Play system sound with custom mp3 file
         if let customSoundUrl = Bundle.main.url(forResource: "beep-07", withExtension: "mp3") {
             var customSoundId: SystemSoundID = 0
             AudioServicesCreateSystemSoundID(customSoundUrl as CFURL, &customSoundId)
             //let systemSoundId: SystemSoundID = 1016  // to play apple's built in sound, no need for upper 3 lines
-            
+
             AudioServicesAddSystemSoundCompletion(customSoundId, nil, nil, { (customSoundId, _) -> Void in
                 AudioServicesDisposeSystemSoundID(customSoundId)
             }, nil)
-            
+
             AudioServicesPlaySystemSound(customSoundId)
         }
-        
+
         // Stop capturing and hence stop executing metadataOutput function over and over again
         captureSession?.stopRunning()
-        
+
         // Call the function which performs navigation and pass the code string value we just detected
         displayDetailsViewController(scannedCode: stringCodeValue)
-        
+
     }
-    
+
     func displayDetailsViewController(scannedCode: String) {
         let detailsViewController = DetailsViewController()
         detailsViewController.scannedCode = scannedCode
