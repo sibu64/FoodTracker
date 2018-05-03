9ca93c50cc458ae88457b59af019a7d541e2c98d
Jean-Michel ZARAGOZA <jmz49xp@gmail.com>
2018-05-03 22:35:57 +0200

    version tableView 0

--- Scanner/DetailsViewController.swift
+++ Scanner/DetailsViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class detailsViewController: UIViewController {
+class detailsViewController: UIViewController, UITableViewDataSource {
 
-        var codeLabel_in: String?
-        var productNameLabel_in: String?
+
+    var codeLabel_in: String?
+    var productNameLabel_in: String?
+    var ingredientsArray:[String] = []
+
+
 
     @IBOutlet weak var codeLabel: UILabel!
     
@@ -19,17 +23,41 @@ class detailsViewController: UIViewController {
     
     @IBOutlet weak var backButton: UIButton!
     
-    
+
+    @IBOutlet weak var ui_tableView: UITableView!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //link de la table avec "son" controller
+        ui_tableView.dataSource = self
 
         backButton.layer.cornerRadius = 25
 
         codeLabel.text = codeLabel_in
         productNameLabel.text = productNameLabel_in
 
+        for ingredient in self.ingredientsArray {
+            print("\(ingredient)")
+        }
+        print(ingredientsArray.count)
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ingredientsArray.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "number-Cell", for: indexPath)
+
+        if let titleLabel = cell.textLabel {
+
+            titleLabel.text = ingredientsArray[indexPath.row]
+        }
+
+        return cell
+    }
 
 }
--- Scanner/OpenFoodViewController.swift
+++ Scanner/OpenFoodViewController.swift
@@ -18,6 +18,7 @@ class OpenFoodViewController: UIViewController {
     var urlStringProduct:String = ""
     var urlStringImageProduct:String = ""
     var codeProduit:String = ""
+    var ingredientsArray:[String] = []
 
     //Codage en dur des parties préfixe et suffixe de l'url
     let urlDeBase = "https://fr.openfoodfacts.org/api/v0/produits/"
@@ -76,33 +77,53 @@ class OpenFoodViewController: UIViewController {
 
                     //ensuite pour parser le fichier JSON.
                     //on descend d'1 cran dans l'arborescence du dictionnaire à partir de la racine
-                    //on trouve et stocke le noeud "product" dans infosProduit
+                    //on trouve et stocke le noeud "product" parmi les "clefs".
                     if let product = reponse["product"] as? [String: AnyObject] {
 
-                            //on descend encore d'1 cran dans l'arborescence à partir de infosProduit
-                            //on trouve et stocke le noeud "product_name_fr" dans nomProduit
-                            if let product_name_fr = product["product_name_fr"] as? String {
+                        //on descend encore d'1 cran dans l'arborescence à partir de "product"
+                        //on trouve et stocke le noeud "product_name_fr" dans "product_name_fr"
+                        if let product_name_fr = product["product_name_fr"] as? String {
 
-                                self.productLabel.text = ("\(product_name_fr)")
-                                //self.productLabel.text = "Monsanto : Le meilleurs produit du monde pour empoisonner tout le monde"
-                                //print ("\(product_name_fr)")
-                            }
+                            self.productLabel.text = ("\(product_name_fr)")
+                            //self.productLabel.text = "Monsanto : Le meilleurs produit du monde pour empoisonner tout le monde"
+                            //print ("\(product_name_fr)")
+                        }
 
-                            //au même niveau sous infosProduit
+                        //au même niveau dans l'arborescence à partir de "product"
+                        //on trouve et stocke le noeud "ingredients_ids_debug" dans "ingredients_ids_debug"
+                        if let ingredients_ids_debug = product["ingredients_ids_debug"] as? [String] {
+
+                            self.ingredientsArray = ingredients_ids_debug
+
+//                            //mappage qui marche
+//                            ingredients_ids_debug.map( { (ingredient) in
+//                                print("\(ingredient)")
+//                            })
+
+//                            for ingredient in self.ingredientsArray {
+//                                print("\(ingredient)")
+//                            }
+
+                            //self.productLabel.text = ("\(product_name_fr)")
+                            //self.productLabel.text = "Monsanto : Le meilleurs produit du monde pour empoisonner tout le monde"
+                            //print ("ingredients_ids_debug : \(ingredients_ids_debug)")
+
+                            //au même niveau dans l'arborescence à partir de "product"
                             //on trouve et stocke le noeud "image_front_url" dans urlImage
                             if let urlImage = product["image_front_url"] as? String {
 
                                 //print ("Image URL : \(urlImage)")
                                 self.urlStringImageProduct = urlImage
+                                //appel fonction
                                 self.requestImageProduct()
-                        }
-                    }
-                }
 
-            } // end of : if let reponse = response.value.
+                            } // end of : if let urlImage = product["image_front_url"] as? String
 
+                        } // end of : if let ingredients_ids_debug = product
+                    } // end of :  if let product = reponse["product"] as? [String: AnyObject]
+                }  // end of : if let reponse = response.value.
+            } // end of :  if response.error == nil
         } // end of : Alamofire.request(url).responseJSON { (response) in
-
     } // end of : func requeteDb()
 
 
@@ -153,9 +174,18 @@ class OpenFoodViewController: UIViewController {
                 if let nextScreen = segue.destination as? detailsViewController {
                     nextScreen.codeLabel_in = codeLabel.text
                     nextScreen.productNameLabel_in = productLabel.text
+                    nextScreen.ingredientsArray = self.ingredientsArray
+                    
                 }
 
         } // end of : if segue.identifier == "segueToOpenFood"
     } // end of : override func prepare(for segue: 
 
 } // class OpenFoodViewController: UIViewController
+
+
+
+
+
+
+
